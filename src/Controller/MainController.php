2ab0b52fd7c554d9d831af4fb09bd6d1562ba63a
Antoine COUSIN <85739437+Antoine-COUSIN@users.noreply.github.com>
2023-09-04 10:55:07 +0200

    Creating the form with collection

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Tag;
+use App\Entity\Task;
+use App\Form\TaskType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('main/index.html.twig');
+        $task = new Task();
+
+        $tag1 = new Tag();
+        $tag1->setName('Tag1');
+        $task->addTag($tag1);
+
+        $tag2 = new Tag();
+        $tag2->setName('Tag2');
+        $task->addTag($tag2);
+
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()){
+            dd($form->getData());
+        }
+
+
+        return $this->render('main/index.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
